1080b43ae7dae833ad4f28659c29339b42cc10d0
krze <kenkrzeminski@gmail.com>
2018-06-26 19:31:17 +0000

    Camera class working

---
+++ security_camera.py
@@ -0,0 +1,22 @@
+from picamera import PiCamera
+from time import sleep
+import datetime
+
+# Camera for capturing
+class SecurityCamera:
+    camera = PiCamera()
+    
+    # Captures 3 pictures in the "security" folder on desktop
+    def capture(self):
+        file_names = []
+        now = datetime.datetime.now()
+
+        for i in range(0,3):
+            name = str(now) + '_' + str(i) + '.jpg'
+            file_names.append(name)
+
+        for file_name in file_names:
+            self.camera.start_preview()
+            sleep(2)
+            self.camera.capture('/home/pi/Desktop/security/' + file_name)
+            self.camera.stop_preview()
