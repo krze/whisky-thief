0c24fd66af04bce09620accf02b2f833f04964b1
krze <kenkrzeminski@gmail.com>
2018-06-26 21:00:43 -0400

    Multi-images sent

--- send_this.py
+++ send_this.py
@@ -5,50 +5,60 @@ from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.MIMEImage import MIMEImage
 
-# Define these once; use them twice!
-strFrom = 'whiskythief1@gmail.com'
-strTo = 'krze@tumblr.com'
+class EmailSender:
+    
+    def send_files(self, files):
+        # Define these once; use them twice!
+        strFrom = 'whiskythief1@gmail.com'
+        strTo = 'krze@tumblr.com'
 
-# Create the root message and fill in the from, to, and subject headers
-msgRoot = MIMEMultipart('related')
-msgRoot['Subject'] = 'test message'
-msgRoot['From'] = strFrom
-msgRoot['To'] = strTo
-msgRoot.preamble = 'This is a multi-part message in MIME format.'
+        # Create the root message and fill in the from, to, and subject headers
+        msgRoot = MIMEMultipart('related')
+        msgRoot['Subject'] = 'test message'
+        msgRoot['From'] = strFrom
+        msgRoot['To'] = strTo
+        msgRoot.preamble = 'This is a multi-part message in MIME format.'
 
-# Encapsulate the plain and HTML versions of the message body in an
-# 'alternative' part, so message agents can decide which they want to display.
-msgAlternative = MIMEMultipart('alternative')
-msgRoot.attach(msgAlternative)
+        # Encapsulate the plain and HTML versions of the message body in an
+        # 'alternative' part, so message agents can decide which they want to display.
+        msgAlternative = MIMEMultipart('alternative')
+        msgRoot.attach(msgAlternative)
 
-msgText = MIMEText('This is the alternative plain text message.')
-msgAlternative.attach(msgText)
+        msgText = MIMEText('This is the alternative plain text message.')
+        msgAlternative.attach(msgText)
 
-# We reference the image in the IMG SRC attribute by the ID we give it below
-msgText = MIMEText('<b>Some <i>HTML</i> text</b> and an image.<br><img src="cid:image1"><br>Nifty!', 'html')
-msgAlternative.attach(msgText)
+        imageNum = 1
+        msgHTML =''
+        imgSrc = '<img src="cid:'
+        
+        for image_dir in files:
+            fp = open(image_dir, 'rb')
+            msgImage = MIMEImage(fp.read())
+            fp.close()
+            imageID = 'image' + str(imageNum)
+                           
+            msgImage.add_header('Content-ID', '<' + imageID + '>')
+            msgRoot.attach(msgImage)
+            msgHTML = msgHTML + '<br>' + imgSrc + imageID + '">'
+            imageNum += 1
+        
+                # We reference the image in the IMG SRC attribute by the ID we give it below
+        msgText = MIMEText(msgHTML, 'html')
+        msgAlternative.attach(msgText)
 
-# This example assumes the image is in the current directory
-fp = open('test.jpg', 'rb')
-msgImage = MIMEImage(fp.read())
-fp.close()
+        password = 'ello'
 
-# Define the image's ID as referenced above
-msgImage.add_header('Content-ID', '<image1>')
-msgRoot.attach(msgImage)
+        with open('password', 'r') as fin:
+            password = fin.read()
 
-password = 'password'
+        # Send the email (this example assumes SMTP authentication is required)
+        import smtplib
+        smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        #smtp.connect('smtp.gmail.com', 465)
+        smtp.ehlo()
+        smtp.login('whiskythief1@gmail.com', password)
+        smtp.sendmail(strFrom, strTo, msgRoot.as_string())
+        smtp.quit()
+    
+    
 
-filename = 'password'
-
-with open(filename, 'r') as fin:
-    password = fin.read()
-
-# Send the email (this example assumes SMTP authentication is required)
-import smtplib
-smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-#smtp.connect('smtp.gmail.com', 465)
-smtp.ehlo()
-smtp.login('whiskythief1@gmail.com', password)
-smtp.sendmail(strFrom, strTo, msgRoot.as_string())
-smtp.quit()
--- whisky_thief.py
+++ whisky_thief.py
@@ -1,4 +1,10 @@
 from security_camera import SecurityCamera
+from send_this import EmailSender
 
-camera = SecurityCamera()
-camera.capture()
+#camera = SecurityCamera()
+#camera.capture()
+
+files = ['test.jpg', 'test2.jpg', 'test3.jpg']
+sender = EmailSender()
+
+sender.send_files(files)
